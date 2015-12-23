081601e7b506347d34874bd8fceeba433c31b452
Necdet Can Atesman <c.atesman@gmx.net>
2015-12-23 10:23:32 +0100

    Better handling for missing alembic
    The alembic support file will now be skipped entirely. This fixes a bug
    in the current implementation (that was providing a stub API) and should
    prevent future problems, too.

--- score/db/alembic.py
+++ score/db/alembic.py
@@ -1,62 +1,54 @@
-from ._sa_stmt import (
-    generate_create_inheritance_view_statement,
-    generate_drop_inheritance_view_statement)
-
 try:
     from alembic.operations import Operations, MigrateOperation
 except ImportError:
+    # No alembic installed, just skip this file as a whole
+    pass
+else:
+    from ._sa_stmt import (
+        generate_create_inheritance_view_statement,
+        generate_drop_inheritance_view_statement)
 
-    class Operations:
-        def register_operation(self, *args, **kwargs):
-            pass
+    @Operations.register_operation("create_inheritance_view")
+    class CreateInheritanceViewOp(MigrateOperation):
+        """Create a View."""
 
-    class MigrateOperation:
-        pass
+        def __init__(self, db_class, schema=None):
+            self.db_class = db_class
+            self.schema = schema
 
+        @classmethod
+        def create_inheritance_view(cls, operations, db_class, **kw):
+            op = CreateInheritanceViewOp(db_class, **kw)
+            return operations.invoke(op)
 
-@Operations.register_operation("create_inheritance_view")
-class CreateInheritanceViewOp(MigrateOperation):
-    """Create a View."""
+        def reverse(self):
+            # only needed to support autogenerate
+            return DropInheritanceViewOp(self.sequence_name, schema=self.schema)
 
-    def __init__(self, db_class, schema=None):
-        self.db_class = db_class
-        self.schema = schema
+    @Operations.register_operation("drop_inheritance_view")
+    class DropInheritanceViewOp(MigrateOperation):
+        """Create a View."""
 
-    @classmethod
-    def create_inheritance_view(cls, operations, db_class, **kw):
-        op = CreateInheritanceViewOp(db_class, **kw)
-        return operations.invoke(op)
+        def __init__(self, db_class, schema=None):
+            self.db_class = db_class
+            self.schema = schema
 
-    def reverse(self):
-        # only needed to support autogenerate
-        return DropInheritanceViewOp(self.sequence_name, schema=self.schema)
+        @classmethod
+        def drop_inheritance_view(cls, operations, db_class, **kw):
+            op = DropInheritanceViewOp(db_class, **kw)
+            return operations.invoke(op)
 
+        def reverse(self):
+            # only needed to support autogenerate
+            return CreateInheritanceViewOp(self.sequence_name,
+                                           schema=self.schema)
 
-@Operations.register_operation("drop_inheritance_view")
-class DropInheritanceViewOp(MigrateOperation):
-    """Create a View."""
+    @Operations.implementation_for(CreateInheritanceViewOp)
+    def create_inheritance_view(operations, op):
+        stmt = generate_create_inheritance_view_statement(op.db_class)
+        operations.execute(stmt)
 
-    def __init__(self, db_class, schema=None):
-        self.db_class = db_class
-        self.schema = schema
-
-    @classmethod
-    def drop_inheritance_view(cls, operations, db_class, **kw):
-        op = DropInheritanceViewOp(db_class, **kw)
-        return operations.invoke(op)
-
-    def reverse(self):
-        # only needed to support autogenerate
-        return CreateInheritanceViewOp(self.sequence_name, schema=self.schema)
-
-
-@Operations.implementation_for(CreateInheritanceViewOp)
-def create_inheritance_view(operations, op):
-    stmt = generate_create_inheritance_view_statement(op.db_class)
-    operations.execute(stmt)
-
-
-@Operations.implementation_for(DropInheritanceViewOp)
-def drop_inheritance_view(operations, op):
-    stmt = generate_drop_inheritance_view_statement(op.db_class)
-    operations.execute(stmt)
+    @Operations.implementation_for(DropInheritanceViewOp)
+    def drop_inheritance_view(operations, op):
+        stmt = generate_drop_inheritance_view_statement(op.db_class)
+        operations.execute(stmt)
