0432733e2dc76e3ea0d64fa3998f14beefdef5ec
Necdet Can Ateşman <can@strg.at>
2016-03-22 14:38:05 +0100

    Release: 0.5.6

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.db',
-    version='0.5.5',
+    version='0.5.6',
     description='Database connectivity for The SCORE Framework',
     long_description=README,
     author='strg.at',
